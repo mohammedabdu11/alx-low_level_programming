fb751daa151d07d28688dd178258411ea8ca28d0
mohammedabdu11 <mabdu322@gmail.com>
2022-10-28 05:26:44 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	int i;
+
+	a = 'a';
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(a <= 'f') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
